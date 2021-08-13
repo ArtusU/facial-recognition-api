bbadff72ccd6b8ac7c46645a7ad8e133b5c3fc7c
ArtusU <artusrock@hotmail.com>
2021-08-13 13:01:31 +0100

    ChangeEmail.js form

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import FileUploadView
+from .views import FileUploadView, ChangeEmailView
 
 app_name = 'core'
 
 
 urlpatterns = [
-    path('demo/', FileUploadView.as_view(), name='file-upload-demo')
+    path('demo/', FileUploadView.as_view(), name='file-upload-demo'),
+    path('change-email/', ChangeEmailView.as_view(), name='change-email')
 ]
--- src/constants.js
+++ src/constants.js
@@ -14,3 +14,4 @@ export const APIEndpoint = `${host}/api`
 
 export const fileUploadURL = `${APIEndpoint}/demo/`;
 export const facialRecognitionURL = `${APIEndpoint}/upload/`;
+export const changeEmailURL = `${APIEndpoint}/change-email/`;
