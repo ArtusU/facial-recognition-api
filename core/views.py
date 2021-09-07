2169ddcfff7c48f5e002719eb9223c69dd3dc907
ArtusU <artusrock@hotmail.com>
2021-09-07 13:02:40 +0100

    throttling

--- core/views.py
+++ core/views.py
@@ -39,6 +39,7 @@ def get_user_from_token(request):
 
 class FileUploadView(APIView):
     permission_classes = (AllowAny, )
+    throttle_scope = 'demo'
 
     def post(self, request, *args, **kwargs):
 
